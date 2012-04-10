bec2f227c1c5e1c722b1872f9765f2186e3f6a77
samsung_se <samsung_se@hotmail.com>
2012-04-10 13:30:56 +0000

    Inserindo classes e relatórios incompletos.

---
+++ CÓDIGO/Academia/Academia/negocio/PagamentoController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Academia.dados;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Academia.negocio
+{
+    public class PagamentoController
+    {
+        private PagamentoDB pagamentoDB = null;
+
+        public PagamentoController()
+        {
+            this.pagamentoDB = new PagamentoDB();
+        }
+
+        public void Inserir(Pagamento pagamento)
+        {
+            int matricula = pagamento.IdMatricula;
+
+            // verifica pagamento atrasado
+            this.PagamentoAtrasado(matricula);
+
+            // efetua o pagamento
+            this.pagamentoDB.Inserir(pagamento);
+        }
+
+        public void Alterar(Pagamento pagamento)
+        {
+            this.pagamentoDB.Alterar(pagamento);
+        }
+
+        public DataTable GetClienteNome(string nome)
+        {
+            return this.pagamentoDB.GetClienteNome(nome);
+        }
+
+        public void PagamentoAtrasado(int id_matricula)
+        {
+            this.pagamentoDB.PagamentoAtrasado(id_matricula);
+        }
+
+
+        public DataTable GetPatamentosCliente(int matricula)
+        {
+            return this.pagamentoDB.GetPagamentosCliente(matricula);
+        }
+    }
+}
